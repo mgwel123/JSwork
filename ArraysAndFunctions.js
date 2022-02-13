2fab30a3233a00764762eb4becf50d133f518cd0
Michael Welker <mikegw224@gmail.com>
2022-02-13 14:41:53 -0700

    created Average Function

--- ArraysAndFunctions.js
+++ ArraysAndFunctions.js
@@ -2,7 +2,22 @@ var ages = [3, 9, 23, 64, 2, 8, 28, 93]
 function subtract(){
     let x = ages[0];
     let y = ages[ages.length - 1];
-    console.log(x-y);
+    console.log(y - x);
 }
 
 subtract();
+
+ages.push (100);
+
+subtract();
+
+function average(){
+    let sum = 0;
+    for (let i = 0; i < ages.length; i++){
+        sum +=ages[i];
+    }
+    return sum / ages.length;
+}
+
+console.log(average());
+
